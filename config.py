217250da8b9adadf59fc3798a503353ee1bf32b7
Notalive77 <katariyachhavi91@gmail.com>
2024-09-03 02:46:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Don't use quotes( " and ' )
-#SCRIPT BY VENOMxCRAZY
+#SCRIPT BY VISHUXBEAST
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7402858862:AAE_Y8N0yRQWU21GDIZ8OwJdzPzMf3yKhcg")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+VISHUXBEAST_USER=("VISHUXBEAST")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6156762330")
 
 
 
